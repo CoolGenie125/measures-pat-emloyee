d94e1025ff360cac75d94186248eab7165eb3f3a
genie125 <geniebaby1024@gmail.com>
2022-06-20 23:27:59 +0900

    add mobile component
    add mobile component in sidebar

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,7 +4,7 @@ import { LayoutStyles } from "./LayoutStyle";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
 import Footer from "./Footer/Footer";
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import { AccountSetting } from "pages/Account/Setting/AccountSetting";
 import { ThankYou } from "pages/ThankYou/ThankYou";
 import { ErrorRoute } from "pages/ErrorRoute/ErrorRoute";
@@ -15,33 +15,11 @@ export default function Layout() {
   const classes = LayoutStyles();
   const [status, setStatus] = useState(true);
 
-  const onclick = () => {
-    setStatus(true);
-  };
-
-  const onSidebarClose = () => {
-    setStatus(false);
-  };
-
-  const handleWindowResize = useCallback((event: any) => {
-    if (window.innerWidth > 839) {
-      setStatus(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, [handleWindowResize]);
-
   return (
     <Router>
-      <Sidebar handleClose={onSidebarClose} mobileStatus={status} />
+      <Sidebar mobileStatus={status} action={()=>setStatus(false)}/>
       <div className={classes.container}>
-        <Header mobileAction={onclick} />
+        <Header mobileAction={()=>setStatus(true)} />
         <Routes>
           <Route path='/' element={<Account />} />
           <Route path='/account' element={<Account />} />
--- src/layout/Sidebar/MobileSidebar/MobileSidebar.tsx
+++ src/layout/Sidebar/MobileSidebar/MobileSidebar.tsx
@@ -1,52 +1,58 @@
-/** @format */
-
-import { SidebarStyles } from "./SidebarStyles";
-import LogoImg from "../../assets/images/logo.webp";
-import avatar from "../../assets/images/avatar.png";
-import banner from "../../assets/images/banner.png";
+import { useStyles } from "./MobileSidebarStyles";
+import LogoImg from "../../../assets/images/logo.webp";
+import avatar from "../../../assets/images/avatar.png";
+import banner from "../../../assets/images/banner.png";
 import { SidbarData } from "config/constant";
-import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import clsx from "clsx";
-import MobileSidebar from "./MobileSidebar/MobileSidebar";
 
-interface SidebarProps {
+interface MobileSidebarContentProps {
   className?: any;
   mobileStatus: boolean;
-  action: () => void;
+  moblieClose: () => void;
 }
 
-export default function Sidebar({
+export default function MobileSidebarContent({
   className,
   mobileStatus,
-  action,
-}: SidebarProps) {
-  const classes = SidebarStyles();
+  moblieClose,
+}: MobileSidebarContentProps) {
+  const classes = useStyles();
   const navigate = useNavigate();
+
+  const handleLink = (e: any) => {
+    moblieClose();
+    navigate(e);
+  };
 
   return (
     <>
-      <div className={clsx(classes.root, className)}>
+      <div
+        className={clsx(
+          classes.root,
+          className,
+          mobileStatus ? classes.displayShow : classes.displayNone
+        )}>
         <img
           src={LogoImg}
           className={classes.logoImg}
           alt='logo'
-          onClick={() => navigate("/")}
+          onClick={() => handleLink("/")}
         />
         <div
           className={classes.avatarRoot}
-          onClick={() => navigate("/account")}>
+          onClick={() => handleLink("/account")}>
           <img src={avatar} className={classes.avatarImg} />
           <div className={classes.avatarName}>しさく くらら</div>
           <div className={classes.avatarJob}>合同会社施策ぱっと</div>
         </div>
-        <div className={classes.sidebarContent}>
+        <div className={classes.mobileSidebarContent}>
           {SidbarData.map((item: any, key: any) => {
             return (
               <div
-                className={classes.sidebarItem}
+                className={classes.mobileSidebarItem}
                 key={key}
-                onClick={() => navigate(item.link)}>
+                onClick={() => handleLink(item.link)}>
                 <img
                   src={item.img}
                   className={classes.itemImg}
@@ -64,7 +70,16 @@ export default function Sidebar({
           <img src={banner} className={classes.banner}></img>
         </div>
       </div>
-      <MobileSidebar mobileStatus={mobileStatus} moblieClose={action} />
+      <div
+        className={mobileStatus ? classes.closePart : classes.displayNone}
+        onClick={moblieClose}>
+        <div className={classes.closeRootIcon}>
+          <i className='fal fa-times'></i>
+        </div>
+      </div>
+      <div className={classes.studyLink}>
+        <img src={banner} className={classes.banner}></img>
+      </div>
     </>
   );
 }
